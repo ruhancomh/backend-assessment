49f9626924e3f75eb1adf7c656994d2faeef5862
Ruhan de Oliveira Baiense <rdgitarra@gmail.com>
2022-06-13 17:33:51 -0300

    refactor: remove ununsed comments

--- app/src/main/server.ts
+++ app/src/main/server.ts
@@ -1,18 +1,9 @@
-// import express from 'express'
-
-// const PORT = 3000
-// const HOST = '0.0.0.0'
-
-// const app = express()
-
-// app.get('/', (req, res) => {
-//   res.send('Hello World')
-// })
-
-// app.listen(PORT, HOST)
-
 import app from './config/app'
 import { env } from './config/env'
+
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
 
 app.listen(
   env.serverPort,
